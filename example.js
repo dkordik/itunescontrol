abbc703b45651e9ed85e2c46a3aa4a70a7b4bd0f
Dan Kordik <dkordik.public@gmail.com>
2013-11-12 02:51:02 -0600

    Update example.js with correct index.js path

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-iTunesControl = require("./itunes-search.js");
+iTunesControl = require("./index.js");
 
 iTunesControl.search(process.argv[2] || "radiohead kid a right place", function (results) {
 
